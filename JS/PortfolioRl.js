01685764618d1166cdf3cf6e684d256e24a2454d
JokaLR4 <louisdistinct@hotmail.fr>
2023-05-09 23:03:15 +0200

    changement
    changement

--- JS/PortfolioRl.js
+++ JS/PortfolioRl.js
@@ -13,3 +13,23 @@ document.getElementById('contact-form').addEventListener('submit', function(even
   var message = document.getElementById('message-input').value; 
 
 });
+
+// Récupérer tous les liens qui pointent vers des sections de la page
+const links = document.querySelectorAll('a[href^="#"]');
+
+// Ajouter un événement click à chaque lien
+links.forEach(link => {
+  link.addEventListener('click', e => {
+    // Empêcher le comportement par défaut du lien
+    e.preventDefault();
+
+    // Récupérer l'élément de la page qui correspond à l'ancre du lien
+    const target = document.querySelector(link.getAttribute('href'));
+
+    // Animer le défilement de la page jusqu'à l'élément cible
+    target.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
