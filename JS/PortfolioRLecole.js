f8328c292751f6232f1eccd6d13a0cee386c03d8
JokaLR4 <louisdistinct@hotmail.fr>
2023-06-05 23:39:32 +0200

    documentations
    documentations

--- JS/PortfolioRLecole.js
+++ JS/PortfolioRLecole.js
@@ -1,10 +1,22 @@
 // Récupération des éléments HTML
 var boiteModale = document.getElementById("boite-modale");
 var boiteModale2 = document.getElementById("boite-modale2");
+var boiteModale3 = document.getElementById("boite-modale3");
+var boiteModale4 = document.getElementById("boite-modale4");
+var boiteModale5 = document.getElementById("boite-modale5");
+var boiteModale6 = document.getElementById("boite-modale6");
 var btnOuvrir = document.getElementById("btn-ouvrir");
 var btnOuvrir2 = document.getElementById("btn-ouvrir2");
+var btnOuvrir3 = document.getElementById("btn-ouvrir3");
+var btnOuvrir4 = document.getElementById("btn-ouvrir4");
+var btnOuvrir5 = document.getElementById("btn-ouvrir5");
+var btnOuvrir6 = document.getElementById("btn-ouvrir6");
 var btnFermer = document.getElementById("btn-fermer");
 var btnFermer2 = document.getElementById("btn-fermer2");
+var btnFermer3 = document.getElementById("btn-fermer3");
+var btnFermer4 = document.getElementById("btn-fermer4");
+var btnFermer5 = document.getElementById("btn-fermer5");
+var btnFermer6 = document.getElementById("btn-fermer6");
 
 // Ajout d'un événement pour ouvrir la boîte modale
 btnOuvrir.onclick = function() {
@@ -15,6 +27,22 @@ btnOuvrir2.onclick = function() {
   boiteModale2.style.display = "block";
 }
 
+btnOuvrir3.onclick = function() {
+  boiteModale3.style.display = "block";
+}
+
+btnOuvrir4.onclick = function() {
+  boiteModale4.style.display = "block";
+}
+
+btnOuvrir5.onclick = function() {
+  boiteModale5.style.display = "block";
+}
+
+btnOuvrir6.onclick = function() {
+  boiteModale6.style.display = "block";
+}
+
 // Ajout d'un événement pour fermer la boîte modale
 btnFermer.onclick = function() {
   boiteModale.style.display = "none";
@@ -23,4 +51,22 @@ btnFermer.onclick = function() {
 // Ajout d'un événement pour fermer la boîte modale
 btnFermer2.onclick = function() {
   boiteModale2.style.display = "none";
+}
+
+// Ajout d'un événement pour fermer la boîte modale
+btnFermer3.onclick = function() {
+  boiteModale3.style.display = "none";
+}
+
+// Ajout d'un événement pour fermer la boîte modale
+btnFermer4.onclick = function() {
+  boiteModale4.style.display = "none";
+}
+
+btnFermer5.onclick = function() {
+  boiteModale5.style.display = "none";
+}
+
+btnFermer6.onclick = function() {
+  boiteModale6.style.display = "none";
 }
--- JS/portfolioRLalternance.js
+++ JS/portfolioRLalternance.js
@@ -113,10 +113,16 @@ function setSlideNo() {
 // Récupération des éléments HTML
 var boiteModale = document.getElementById("boite-modale");
 var boiteModale2 = document.getElementById("boite-modale2");
+var boiteModale3 = document.getElementById("boite-modale3");
+var boiteModale4 = document.getElementById("boite-modale4");
 var btnOuvrir = document.getElementById("btn-ouvrir");
 var btnOuvrir2 = document.getElementById("btn-ouvrir2");
+var btnOuvrir3 = document.getElementById("btn-ouvrir3");
+var btnOuvrir4 = document.getElementById("btn-ouvrir4");
 var btnFermer = document.getElementById("btn-fermer");
 var btnFermer2 = document.getElementById("btn-fermer2");
+var btnFermer3 = document.getElementById("btn-fermer3");
+var btnFermer4 = document.getElementById("btn-fermer4");
 
 // Ajout d'un événement pour ouvrir la boîte modale
 btnOuvrir.onclick = function() {
@@ -127,6 +133,14 @@ btnOuvrir2.onclick = function() {
   boiteModale2.style.display = "block";
 }
 
+btnOuvrir3.onclick = function() {
+  boiteModale3.style.display = "block";
+}
+
+btnOuvrir4.onclick = function() {
+  boiteModale4.style.display = "block";
+}
+
 // Ajout d'un événement pour fermer la boîte modale
 btnFermer.onclick = function() {
   boiteModale.style.display = "none";
@@ -135,4 +149,14 @@ btnFermer.onclick = function() {
 // Ajout d'un événement pour fermer la boîte modale
 btnFermer2.onclick = function() {
   boiteModale2.style.display = "none";
+}
+
+// Ajout d'un événement pour fermer la boîte modale
+btnFermer3.onclick = function() {
+  boiteModale3.style.display = "none";
+}
+
+// Ajout d'un événement pour fermer la boîte modale
+btnFermer4.onclick = function() {
+  boiteModale4.style.display = "none";
 }
